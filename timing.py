348c9938b37ea40809cf1fc61bd5da3199343a58
gregusio <grzegorzdynaq@gmail.com>
2024-12-11 12:50:39 +0100

    Change timing precision

--- timing.py
+++ timing.py
@@ -8,7 +8,7 @@ def timing(f):
         start_time = time()
         res = f(*args, **kwargs)
         end_time = time()
-        print(f'Elapsed time: {end_time - start_time} sec')
+        print(f'Elapsed time: {round((end_time - start_time) * 1000, 2)} ms')
         return res
     
     return wrapper
